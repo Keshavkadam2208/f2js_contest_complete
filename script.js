3c095ec8a851ada2df2b028c9a00d706c8eb74f7
keshav kadam <kkeshav2208@gmail.com>
2025-05-18 20:30:36 +0530

    completed all the task as per given

--- script.js
+++ script.js
@@ -8,20 +8,54 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map((employee) => {
+    if(employee.profession === "developer")
+    {
+      console.log(employee)
+    }
+  });
 }
-
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.map((employee) => {
+    if(employee.profession === "developer")
+    {
+      console.log(employee)
+    }
+  });
 }
+
 
 function addData() {
   //Write your code here, just console.log
-}
+  const newEmployee = {
+      id: 7,
+      name: "keshav",
+      age:"23",
+      profession: "tester",
+      };
+     arr.push(newEmployee);
+     console.log(arr);
+
+  }
+  
+
 
 function removeAdmin() {
   //Write your code here, just console.log
+  const filterdArr = arr.filter((employee) => employee.profession !== "admin");
+  console.log(filterdArr);
 }
+
 
 function concatenateArray() {
   //Write your code here, just console.log
+   const newEmployees = [
+    { id: 5, name: "alice", age: "22", profession: "designer" },
+    { id: 6, name: "bob", age: "25", profession: "manager" },
+    { id: 7, name: "eve", age: "21", profession: "developer" },
+  ];
+  const combined = arr.concat(newEmployees);
+  console.log(combined);
 }
+
